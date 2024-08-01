41b81868a798f750f68112738db954438793cc7b
sohammondal <soham.pf.mondal@gmail.com>
2024-08-02 01:48:50 +0200

    feat: add Accordion

---
+++ components/atoms/Accordion/Accordion.test.tsx
@@ -0,0 +1,30 @@
+import { fireEvent, render, screen } from "@app/utils/test";
+
+import { Accordion } from "./Accordion";
+
+describe("Accordion", () => {
+  it("should toggle content when clicked", async () => {
+    const title = "Section 1";
+    const content = "This is the content for section 1.";
+
+    render(<Accordion title={title}>{content}</Accordion>);
+
+    const header = screen.getByText(title);
+    const getContent = () => screen.queryByText(content);
+
+    expect(header).toBeVisible();
+
+    // should be collapsed by default
+    expect(getContent()).not.toBeInTheDocument();
+
+    // click to show content
+    fireEvent.click(header);
+
+    // should expand after header is clicked
+    expect(screen.getByText(content)).toBeInTheDocument();
+
+    // click to hide content
+    fireEvent.click(header);
+    expect(getContent()).not.toBeInTheDocument();
+  });
+});
