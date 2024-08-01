72c3e3f6417abfdb075116939e0ba0c3ee097d77
sohammondal <soham.pf.mondal@gmail.com>
2024-08-01 21:55:35 +0200

    test: setup jest and react testing library

---
+++ utils/test/index.tsx
@@ -0,0 +1,15 @@
+import React, { ReactElement } from "react";
+
+import { render, RenderOptions } from "@testing-library/react";
+
+const Wrapper = ({ children }: { children: React.ReactNode }) => {
+  return <React.Fragment>{children}</React.Fragment>;
+};
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, "wrapper">
+) => render(ui, { wrapper: Wrapper, ...options });
+
+export * from "@testing-library/react";
+export { customRender as render };
