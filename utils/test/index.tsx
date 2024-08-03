fd585d4aa82e6bd702a1f3631743f7e9318d3fa7
sohammondal <soham.pf.mondal@gmail.com>
2024-08-03 17:41:49 +0200

    test: add integration tests for Home page

--- utils/test/index.tsx
+++ utils/test/index.tsx
@@ -1,17 +1,34 @@
 import React, { ReactElement } from "react";
+import { Provider } from "react-redux";
 
+import { RootState } from "@app/store";
+import { rootReducer } from "@app/store/reducers";
+import { configureStore } from "@reduxjs/toolkit";
 import { render, RenderOptions } from "@testing-library/react";
 
 import "@testing-library/jest-dom";
 
-const Wrapper = ({ children }: { children: React.ReactNode }) => {
-  return <React.Fragment>{children}</React.Fragment>;
+type DeepPartial<T> = {
+  [P in keyof T]?: DeepPartial<T[P]>;
 };
 
 const customRender = (
   ui: ReactElement,
-  options?: Omit<RenderOptions, "wrapper">
-) => render(ui, { wrapper: Wrapper, ...options });
+  options?: Omit<RenderOptions, "wrapper"> & {
+    initialState: DeepPartial<RootState>;
+  }
+) => {
+  const store = configureStore({
+    preloadedState: options?.initialState as RootState,
+    reducer: rootReducer,
+  });
+
+  const Wrapper = ({ children }: { children: React.ReactNode }) => {
+    return <Provider store={store}>{children}</Provider>;
+  };
+
+  return render(ui, { wrapper: Wrapper, ...options });
+};
 
 export * from "@testing-library/react";
 export * from "@testing-library/user-event";
